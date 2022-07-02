8161ac00fde883f31996c5702291062c1a736003
Amunuki <abylalu@gmail.com>
2022-07-02 14:18:53 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
-/**
+
+
+
+/
+
  * main - entry point,
  *
- * Return:  0 after printing the function
- */
+ *  *
+ *
+ *   * Return:  0
+ *
+ *    */
+
+
+
  int main(void)
+
 {
- puts("\"Programming is like building a multilingual puzzle");
+
+	  puts("\"Programming is like building a multilingual puzzle");
 
 	    return (0);
 
