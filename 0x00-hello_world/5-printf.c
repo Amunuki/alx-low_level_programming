0b01f9be48f8469a3c5a451d65487551cd073e17
Amunuki <abylalu@gmail.com>
2022-07-03 08:00:40 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,27 @@
+filename: 5-printf.c
+
+#include <stdio.h>
+
+
+
+/
+
+ * main - prints some text
+ *
+ *  *
+ *
+ *   * Return: always 0(success)
+ *
+ */
+
+
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
