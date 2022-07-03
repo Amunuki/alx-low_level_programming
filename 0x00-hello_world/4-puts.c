5d317b5f2849123d7ce23b3b38ea2d1f3e7237c1
Amunuki <abylalu@gmail.com>
2022-07-03 07:43:34 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-/** 
+/**
  *main - Entry point
  *Return: Always 0 (Succusse)
  */
@@ -10,7 +10,7 @@ int main(void)
 {
 	char str[] = "\"Programming is like building multilingual puzzle";
 
-	puts (str);
+	puts(str);
 
 	return (0);
 }
