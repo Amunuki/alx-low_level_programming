e08f0cdb7bcc7640840c3a906b851a512ec75a10
Amunuki <abylalu@gmail.com>
2022-07-09 12:12:54 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- *
- *  * times_table - prints the 9 times table, starting with 0
+ * times_table - prints the 9 times table, starting with 0
  *
  */
 void times_table(void)
@@ -32,4 +31,4 @@ _putchar((k % 10) + '0');
 }
 _putchar('\n');
 }
-
+}
