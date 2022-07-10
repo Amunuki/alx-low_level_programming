ed552c7e83a2d393eac2733669a27ae295631f3e
Amunuki <abylalu@gmail.com>
2022-07-10 11:43:56 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-* main - tests function that prints if integer is positive or negative
+*main - tests function that prints if integer is positive or negative
 *
-* * Return: 0
+*Return: 0
 *
 */
 int main(void)
@@ -11,5 +11,4 @@ int i;
 i = 0;
 positive_or_negative(i);
 return (0);
-
 }
