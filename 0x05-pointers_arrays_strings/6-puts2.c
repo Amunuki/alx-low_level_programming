81c8232480dbe3071eb478a8503150bd98ee32b5
Amunuki <abylalu@gmail.com>
2022-07-15 07:54:37 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * puts2 - prints every other character
+ * @str: pointer
+ * Return: void
+ */
+void puts2(char *str)
+{
+int i, l;
+l = strlen(str);
+for (i = 0; i < 1; i += 2)
+{
+_putchar(*(str + i));
+}
+_putchar('\n');
+}
+
