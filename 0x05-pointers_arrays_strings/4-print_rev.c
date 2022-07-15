8df7937fb62b69844e09877efb4f20c4b2025af3
Amunuki <abylalu@gmail.com>
2022-07-15 06:45:38 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include <stdiio.h>
+#include "main.h"
+
+/**
+ * print_rev - prints the reverse
+ * @s: pointer
+ * Return: void
+ */
+void print_rev(char *s)
+{
+int i = 0;
+while (s[i])
+i++;
+while (i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
